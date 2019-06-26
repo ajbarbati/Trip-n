a069b96d3def661b885f2c58e5d1992aeda9fbeb
Alex B <ajbarbati@gmail.com>
2019-06-25 21:21:12 -0400

    added different url

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,12 @@
 
 function searchPinterest() {
 //    const searchTerm = "Haiwaii" 
-   const pinterestAPI = "https://cors-anywhere.herokuapp.com/https://api.pinterest.com/v3/search/pins/?join=via_pinner,board,pinner&page_size=50&query=Red&access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
+   const pinterestAPI = "https://cors-anywhere.herokuapp.com/https://api.pinterest.com/v1/boards/ajbarbati/Office-room/pins/?access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
     $.ajax({
         url: pinterestAPI,
         method: 'GET', 
     }).done(function  (response) {
-        console.log(response)
+        console.log(response.data)
     })
 }
 
