19ff4d4734bc192c43ab7c9012056fc528138b8b
Alex B <ajbarbati@gmail.com>
2019-06-26 20:38:16 -0400

    bug

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 
 function searchPinterest() {
 //    const searchTerm = "Haiwaii" 
-   const pinterestAPI = "https://cors-anywhere.herokuapp.com/https://api.pinterest.com/v3/search/pins/?<%=test%>/?access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
+   const pinterestAPI = "https://cors-anywhere.herokuapp.com/https://api.pinterest.com/v1/search/pins/?<%=test%>/?access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
     $.ajax({
         url: pinterestAPI,
         method: 'GET', 
