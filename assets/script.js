3ee376fdbe969eea8635f17246fa692de2c7e6ef
Alex B <ajbarbati@gmail.com>
2019-06-27 19:31:34 -0400

    changed around a few thing having trouble jquery

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
+$(document).ready(function () {
 
 
 function searchPinterest() {
 //    const searchTerm = "Haiwaii" 
    const pinterestAPI = "https://peaceful-caverns-21751.herokuapp.com/https://api.pinterest.com/v1/pins/ajbarbati/Office-room/pins/?access_token=AjjywUkw8NjFan2xpaYaPsBtxnuQFawwLyNrmSJF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
+   
     $.ajax({
         url: pinterestAPI,
         method: 'GET', 
     }).done(function  (response) {
-        console.log(response.data)
-
+        console.log(response)
+        console.log('buts')
+        
         // var pURL = response.data.url
 
         // $(pURL).appendTo('#pinterest')
@@ -16,7 +19,7 @@ function searchPinterest() {
 }
 
 searchPinterest()
-
+})
 // createPin: function (callback)  {
 //     PDK.request('')
 // }
