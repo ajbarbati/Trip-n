5984c9c80209fd2671dbd7564b78d1841a1156cc
Alex B <ajbarbati@gmail.com>
2019-06-27 19:01:30 -0400

    ..

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 
 function searchPinterest() {
 //    const searchTerm = "Haiwaii" 
-   const pinterestAPI = "https://api.pinterest.com/v1/pins/ajbarbati/Office-room/?access_token=AjjywUkw8NjFan2xpaYaPsBtxnuQFawwLyNrmSJF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
+   const pinterestAPI = "https://api.pinterest.com/v1/pins/ajbarbati/Office-room/pins/?access_token=AjjywUkw8NjFan2xpaYaPsBtxnuQFawwLyNrmSJF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
     $.ajax({
         url: pinterestAPI,
         method: 'GET', 
