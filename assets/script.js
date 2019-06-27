97663eae57d976f7ef985f6fb8bbb25074f67c95
Alex B <ajbarbati@gmail.com>
2019-06-27 18:53:57 -0400

    ..

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 
 function searchPinterest() {
 //    const searchTerm = "Haiwaii" 
-   const pinterestAPI = "https://peaceful-caverns-21751.herokuapp.com/https://api.pinterest.com/v1/pins/ajbarbati/Office-room/?access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
+   const pinterestAPI = "https://cors-anywhere.herokuapp.com/https://api.pinterest.com/v1/pins/ajbarbati/Office-room/?access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
     $.ajax({
         url: pinterestAPI,
         method: 'GET', 
