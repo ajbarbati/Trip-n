aa2ca0aeecc8f98a30faba7ba6e472011a90083c
Alex B <ajbarbati@gmail.com>
2019-06-26 20:52:50 -0400

    trying to console log object things

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ function searchPinterest() {
         url: pinterestAPI,
         method: 'GET', 
     }).done(function  (response) {
-        console.log(response.data)
+        console.log(response.data.Array[0].note)
     })
 }
 
