d4ad1f557f32797b7e0b91b3e7416ac6c8816291
Alex B <ajbarbati@gmail.com>
2019-06-26 22:12:38 -0400

    trying to display URL

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,16 @@
 
 function searchPinterest() {
 //    const searchTerm = "Haiwaii" 
-   const pinterestAPI = "https://cors-anywhere.herokuapp.com/https://api.pinterest.com/v1/pins/ajbarbati/?access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
+   const pinterestAPI = "https://cors-anywhere.herokuapp.com/https://api.pinterest.com/v1/pins/ajbarbati/Office-room/?access_token=AsaIpkQDLE1J0v5SO_-6sLCPqHYyFauaSAZjZTxF863V-2C9uAgXQDAAAb0HRfO9dLuAznMAAAAA"
     $.ajax({
         url: pinterestAPI,
         method: 'GET', 
     }).done(function  (response) {
         console.log(response.data.url)
+
+        const pURL = response.data.url
+
+        $("#pinterest").append(pURL)
     })
 }
 
